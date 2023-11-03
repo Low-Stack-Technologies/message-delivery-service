f5427d6a2968101af6439097254af1cb69e5e1ec
Esaias Westberg <esaias@westbergs.se>
2023-11-03 15:03:12 +0100

    Use CORS configuration

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -1,8 +1,14 @@
 import { NextFunction, Request, Response } from 'express'
+import ConfigurationService from '../services/configuration'
 
 const cors = (req: Request, res: Response, next: NextFunction) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Methods', '*')
+  const config = ConfigurationService.get().http.security.cors
+
+  if (config.enabled) {
+    res.header('Access-Control-Allow-Origin', config.origin)
+    res.header('Access-Control-Allow-Methods', config.methods)
+    res.header('Access-Control-Allow-Headers', config.headers)
+  }
 
   next()
 }
